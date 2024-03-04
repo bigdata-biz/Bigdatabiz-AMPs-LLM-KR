102a02d193fa7d07fd4d9180e7f28bc312f361ff
hyeongyu_yeo <hyeongyu_yeo1@kolon.com>
2024-03-04 14:14:23 +0900

    test

--- 2_job-load-models/load_models.sh
+++ 2_job-load-models/load_models.sh
@@ -1,14 +1,19 @@
 # Copy Pre-Downloaded LLM models
 rm -rf ./models
 mkdir models
-cd models
-
-cp -r /tmp/models/eng-sample/* .
-tar -xvf ./embedding-model.tar
-tar -xvf ./llm-model.tar
-rm ./*.tar
-
-# Copy Pre-Downloaded Translation models
 rm -rf ~/.cache/huggingface/hub
 mkdir -p ~/.cache/huggingface/hub
-cp -r /tmp/models/translation-sample/* ~/.cache/huggingface/hub/
+
+# Copy pre downloaded models
+cd models
+cp -r /root/hyeongyu/models/eng-sample/* .
+tar -xvf embedding-model.tar
+tar -xvf llm-model.tar
+rm embedding-model.tar
+rm llm-model.tar
+
+# Copy Pre-Downloaded Translation models
+cd ~/.cache/huggingface/
+cp /root/hyeongyu/models/translation-sample/hub.tar .
+tar -xvf hub.tar
+tar hub.tar
