5e6ff26a515c6acec0e74f03f98acd90cca93f05
bigdata-biz <156773877+bigdata-biz@users.noreply.github.com>
2024-03-04 15:25:43 +0900

    Update load_models.sh
    test

--- 2_job-load-models/load_models.sh
+++ 2_job-load-models/load_models.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.cache/huggingface/hub
 
 # Copy pre downloaded models
 cd models
-cp -r /root/hyeongyu/models/eng-sample/* .
+cp -r /data/cdsw-sample/models/eng-sample/* .
 tar -xvf embedding-model.tar
 tar -xvf llm-model.tar
 rm embedding-model.tar
@@ -14,6 +14,6 @@ rm llm-model.tar
 
 # Copy Pre-Downloaded Translation models
 cd ~/.cache/huggingface/
-cp /root/hyeongyu/models/translation-sample/hub.tar .
+cp /data/cdsw-sample/models/translation-sample/hub.tar .
 tar -xvf hub.tar
 tar hub.tar
